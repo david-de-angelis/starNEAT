9ef032377203b20eebe3259f4016426b2657a1f5
David De Angelis <daviddeangelis98@gmail.com>
2022-03-26 20:40:12 +1100

    fix: python 'gotcha'
    having a default value of a function a mutable value means that other instances will share the same value... which is not desired in this scenario

--- starNEAT/Lobe.py
+++ starNEAT/Lobe.py
@@ -1,8 +1,14 @@
+import string
 from neat import DefaultGenome
+from neat.six_util import iteritems, iterkeys
 
 class Lobe(DefaultGenome):
   
-  def __init__(self, name, connections = {}, nodes = {}):
-    self.name = name
-    self.connections = connections
-    self.nodes = nodes
+    def __init__(self, name, connections, nodes):
+      assert type(name) == str
+
+      self.key = name
+      self.name = name
+      self.connections = connections if connections != None else {}
+      self.nodes = nodes if nodes != None else {}
+      self.fitness = None
