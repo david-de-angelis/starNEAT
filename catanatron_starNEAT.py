da29eaf6836c6bd5eb37b2ca105c1c3ec2c39ebb
David De Angelis <daviddeangelis98@gmail.com>
2022-04-19 01:46:08 +1000

    feat: removed all general starNEAT logic from catan-context-specific file
    and added dynamic catan gameplay logic

--- catanatron_starNEAT.py
+++ catanatron_starNEAT.py
@@ -1,118 +1,144 @@
 from __future__ import print_function
+from catanatron.game import Color, Game
+from catanatron.models.player import RandomPlayer
+from catanatron.state_functions import player_key
+from starNEAT.starNEAT import starNEAT
 
 import os
 import sys
 import neat
+import random
 import visualise.visualise as visualise
 
-from starNEAT.BrainGenome import BrainGenome
-from starNEAT.nn.FeedForward import FeedForward
+from MyPlayer import MyBrain
+from MyPlayer import MyPlayer
 
-# 2-input XOR inputs and expected outputs.
-xor_inputs = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
-xor_outputs = [   (0.0,),     (1.0,),     (1.0,),     (0.0,)]
+class Experiment(starNEAT):
+  lobes = ["move_robber", "build_city", "build_road", "build_settlement"]
 
-class Experiment():
-    ### Catan Constants ###
-    move_robber_id = "move_robber"
-    build_city_id = "build_city"
-    build_road_id = "build_road"
-    build_settlement_id = "build_settlement"
-
-    def __init__(self, config_file_path, epochs):
-        ### starNEAT Parameters ###
-        self.config_file_path = config_file_path
-        self.population = None
-        self.epochs = epochs
-        self.statistics_reporter = None
-        self.neural_network_type = FeedForward
-        self.config = neat.Config(BrainGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, self.config_file_path)
-    
-        ### Catan Parameters ###
-        self.lobe_move_robber_config = None
-        self.lobe_build_city_config = None
-        self.lobe_build_road_config = None
-        self.lobe_build_settlement_config = None
-
-    def run(self):
-        # Create the population, which is the top-level object for a NEAT run.
-        self.population = neat.Population(self.config)
-
-        # Add execution reporters (monitors the execution throughout its lifetime)
-        self.add_reporters()
-
-        # Execute the evaluation & evolution.
-        best_genome = self.population.run(self.evaluate_genomes, self.epochs)
-
-        self.wrap_up(best_genome)
+  def __init__(self, config_file_path, epochs):
+    super().__init__(config_file_path, epochs)
 
 
-    def add_reporters(self):
-        self.statistics_reporter = neat.StatisticsReporter()
-        reporters = [
-            neat.StdOutReporter(True),
-            self.statistics_reporter,
-            neat.Checkpointer(5)
-        ]
+  """
+    Before Run: Add Reporters
+    After Run: Give Summary
 
-        for reporter in reporters:
-            self.population.add_reporter(reporter)
-
-
-    def evaluate_genomes(self, genomes, global_config):
-        for genome_id, genome in genomes:
-            self.evaluate_genome(genome, global_config.genome_config)
-
-
-    def evaluate_genome(self, genome, genome_config):
-        #Establishing required variables
-        self.set_lobe_config_cache(genome_config)
-        lobe_move_robber = genome.lobes[Experiment.move_robber_id]
-        lobe_build_city = genome.lobes[Experiment.build_city_id]
-        lobe_build_road = genome.lobes[Experiment.build_road_id]
-        lobe_build_settlement = genome.lobes[Experiment.build_settlement_id]
-
-        #Evaluating Genome
-        genome.fitness = 4.0
-        nn_move_robber = self.neural_network_type.create(lobe_move_robber, self.lobe_move_robber_config) #remember that this is currently only creating one lobe...
-        for xi, xo in zip(xor_inputs, xor_outputs):
-            output = nn_move_robber.activate(xi)
-            # should probably also set the same fitness value for the lobes, 
-            # (or if the use-case merits individual fitness, then that is also possible, 
-            # however it would only impact the cross_over, and not the gene_pool selection itself)
-            genome.fitness -= (output[0] - xo[0]) ** 2 
+    Return:
+      best_genome (starNeat.BrainGenome.BrainGenome): the best genome in the evolved population
+  """
+  def run(self):
+    # Add execution reporters (monitors the execution throughout its lifetime)
+    self.add_reporters()
+    best_genome = super().run()
+    #perform post-run analysis
+    self.wrap_up(best_genome)
+    return best_genome
 
 
-    def set_lobe_config_cache(self, genome_config):
-        if (self.lobe_move_robber_config == None):
-            self.lobe_move_robber_config = genome_config.brain_lobes_config[Experiment.move_robber_id]
+  def add_reporters(self):
+    self.statistics_reporter = neat.StatisticsReporter()
+    reporters = [
+      neat.StdOutReporter(True),
+      self.statistics_reporter,
+      neat.Checkpointer(5)
+    ]
 
-        if (self.lobe_build_city_config == None):
-            self.lobe_build_city_config = genome_config.brain_lobes_config[Experiment.build_city_id]
-
-        if (self.lobe_build_road_config == None):
-            self.lobe_build_road_config = genome_config.brain_lobes_config[Experiment.build_road_id]
-            
-        if (self.lobe_build_settlement_config == None):
-            self.lobe_build_settlement_config = genome_config.brain_lobes_config[Experiment.build_settlement_id]
+    for reporter in reporters:
+      self.population.add_reporter(reporter)
 
 
-    def wrap_up(self, best_genome):
-      ## Display the winning genome.
-      # print('\nBest genome:\n{!s}'.format(best_genome))
+  def evaluate_genomes(self, genomes, global_config):
+    # Get the same (type of) opponent for all of the population
+    opponent_type = self.get_worthy_opponent_type()
+    for genome_id, genome in genomes:
+      num_games = 1 # should we only play one game per genome to evaluate the population?
+      self.evaluate_genome(genome, global_config.genome_config, opponent_type, num_games=num_games)
 
-      # Show output of the most fit genome against training data.
-      print('\nOutput:')
-      self.formally_evaluate_best_genome(best_genome)
+
+  """
+    TODO: consider making the opponent harder as the population becomes more evolved.
+    Return: A valid catan opponent type
+  """
+  def get_worthy_opponent_type(self): 
+    return RandomPlayer
+
+
+  """
+    create an agent from the brain
+    create a game environment
+    place the agent in the game environment
+    calculate the agent's fitness within the environment
+
+    Return:
+      games_won (int): the number of games won
+  """
+  def evaluate_genome(self, genome, genome_config, opponent_type, num_games = 1): 
+    brain = MyBrain(genome, genome_config, self.neural_network_type, self.lobes)
+
+    games_won = 0
+    player_cumulative_fitness = 0.0
+
+    for i in range(num_games):
+      game = self.play_game(brain, opponent_type)
+      # should maybe give an additional bonus if they are the actual winning_colour...
+      player_cumulative_fitness += self.get_fitness_from_game_state(game, Color.BLUE) #User's player should always be BLUE.
+      if game.winning_color() == Color.BLUE:
+        games_won += 1
       
-      # node_names = {-1:'A', -2: 'B', 0:'A XOR B'}
-      # visualise.draw_net(self.config, best_genome, True, node_names=node_names)
-      visualise.plot_stats(self.statistics_reporter, ylog=False, view=True)
-      visualise.plot_species(self.statistics_reporter, view=True)
 
-      # # How to restore a population from a checkpoint:
-      # population = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
-      # population.run(self.evaluate_genomes, 10)
+    # any value above 10 is effectively equal to 10 in the game of Catan, 
+    # all are considered a win, none of which is better than the other.
+    genome.fitness = min(player_cumulative_fitness / num_games, 10.0)
+    return games_won
+
+
+  def play_game(self, brain, opponent_type):
+    player = MyPlayer(brain, Color.BLUE) #User's player should always be BLUE.
+    opponent = opponent_type(Color.RED)
+
+    # player-order impacts win-likelihood significantly, especially with two players (as opposed to 4).
+    players = [ player, opponent ]
+    random.shuffle(players)
+
+    game = Game(players)
+    winning_colour = game.play()
+
+    #player_fitness = self.get_fitness_from_game_state(game, player.color)
+    #opponent_fitness = self.get_fitness_from_game_state(game, opponent.color)
+
+    # print("Winning colour:", winning_colour, "PLAYER FITNESS: ", player_fitness, "OPPONENT FITNESS: ", opponent_fitness)
+    return game
+
+
+  @staticmethod
+  def get_fitness_from_game_state(game, colour):
+    key = player_key(game.state, colour)
+    return game.state.player_state[f"{key}_ACTUAL_VICTORY_POINTS"]
+
+
+  def formally_evaluate_best_genome(self, best_genome):
+    games_won = self.evaluate_genome(best_genome, self.config.genome_config, RandomPlayer, 10)
+    print("The best genome won", games_won, "games out of 10 against a RandomPlayer!")
+
+
+  def wrap_up(self, best_genome):
+    ## Display the winning genome.
+    # print('\nBest genome:\n{!s}'.format(best_genome))
+
+    # Show output of the most fit genome against training data.
+    print('\nOutput:')
+    self.formally_evaluate_best_genome(best_genome)
+    
+    # node_names = {-1:'A', -2: 'B', 0:'A XOR B'}
+    # visualise.draw_net(self.config, best_genome, True, node_names=node_names)
+    visualise.plot_stats(self.statistics_reporter, ylog=False, view=True)
+    visualise.plot_species(self.statistics_reporter, view=True)
+
+    # # How to restore a population from a checkpoint:
+    # population = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
+    # population.run(self.evaluate_genomes, 10)
+
 
 if __name__ == '__main__':
   # Determine path to configuration file. This path manipulation is
@@ -123,3 +149,4 @@ if __name__ == '__main__':
 
   epochs = int(sys.argv[1]) if len(sys.argv) > 1 else 20
   Experiment(config_path, epochs).run()
+  
