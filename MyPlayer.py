84a8a8b848615cecbf2d75ece1eb4df6b0c292c1
David De Angelis <daviddeangelis98@gmail.com>
2022-05-10 01:20:41 +1000

    feat: add neural network output serialisation and action mapping logic

--- MyPlayer.py
+++ MyPlayer.py
@@ -1,7 +1,9 @@
+import itertools
 from typing import Iterable
 from catanatron.game import Action, Color, Game, Player
-from catanatron.models.enums import ActionPrompt
+from catanatron.models.enums import ActionPrompt, ActionType, Action
 from starNEAT.BrainEmulator import EmulatedBrain
+import constants
 import custom_state_functions
 
 class MyPlayer(Player):
@@ -21,6 +23,41 @@ class MyBrain(EmulatedBrain):
     def __init__(self, genome, genome_config, neural_network_type, lobes):
         super().__init__(genome, genome_config, neural_network_type, lobes)
 
+    def generate_playable_actions_info(self, playable_actions):
+      playable_action_types_seen = constants.action_type_seen_template.copy()
+      playable_actions_id_set = set()
+      for playable_action in playable_actions:
+        print("playable_action")
+        print(playable_action)
+        print(playable_actions)
+        action_type = playable_action.action_type
+        playable_action_types_seen[action_type] = True
+        action_skeleton = (action_type, playable_action.value)
+        action_id = constants.action_skeleton_to_action_id[action_skeleton]
+        playable_actions_id_set.add(action_id)
+      
+      return (playable_actions_id_set, playable_action_types_seen)
+
+
+    def get_best_valid_action(self, output_array, action_type: ActionType, playable_actions_id_set: set):
+      output_dictionary = {index: element for index, element in enumerate(output_array)} # map each value to its index in the array
+      # print("output_dictionary")
+      # print(output_dictionary)
+      action_skeleton = None
+      action_type_offset = constants.action_type_offsets[action_type]
+      for index in sorted(output_dictionary, key=output_dictionary.get, reverse=True): # enumerate over the array by grabbing the key/index of the entries with the highest 'value' first
+        # print(index, output_dictionary[index])
+        action_id = action_type_offset + index
+        if action_id in playable_actions_id_set:
+          action_skeleton = constants.action_id_to_action_skeleton[action_id]
+          break
+
+      if action_skeleton != None:
+        assert action_skeleton[0] == ActionType.BUILD_SETTLEMENT
+        action = custom_state_functions.get_action_from_action_skeleton(self.color, action_skeleton)
+        return action
+      else:
+        return None
 
     """ 
       Based on the perceived environment take an action...
@@ -39,27 +76,20 @@ class MyBrain(EmulatedBrain):
     def decide(self, game: Game, playable_actions: Iterable[Action]):
       # list of all actions https://catanatron.readthedocs.io/en/latest/catanatron_gym.envs.html?highlight=actions#catanatron_gym.envs.catanatron_env.CatanatronEnv.get_valid_actions
       # https://catanatron.readthedocs.io/en/latest/catanatron.html#module-catanatron.state
-      # game.state.board
-      # game.state.player_state
+      
       num_playable_actions = len(playable_actions)
-      # if (num_playable_actions <= 1):
-      #   return playable_actions[0] if num_playable_actions == 1 else None
+      if (num_playable_actions <= 1):
+        return playable_actions[0] if num_playable_actions == 1 else None
 
-
-      print("----")
-      print(game.state.board.roads)
-      print()
-
+      playable_actions_id_set, playable_action_types_seen = self.generate_playable_actions_info(playable_actions)
+      print(playable_action_types_seen)
 
       if (game.state.is_initial_build_phase): #probably put me last, since I'll only ever be needed once per game...
-        # print("game.state.board.buildings")
-        # print(game.state.board.buildings)
-        print("is_initial_build_phase")
-        # print(playable_actions)
-        # print(game.state.player_state)
         if (game.state.current_prompt == ActionPrompt.BUILD_INITIAL_SETTLEMENT):
-          
-          print("is_initial_build_phase.build_initial_settlement")
+          action = self.build_settlement(game, playable_actions_id_set)
+          print("action chosen:")
+          print(action)
+          return action
 
         elif (game.state.current_prompt == ActionPrompt.BUILD_INITIAL_ROAD):
           print("is_initial_build_phase.build_inital_road")
@@ -72,8 +102,6 @@ class MyBrain(EmulatedBrain):
         print("is_road_building")
 
       return playable_actions[0]
-      
-
 
       # raise NotImplementedError("still needs to be developed")
       #e.g.
@@ -93,12 +121,27 @@ class MyBrain(EmulatedBrain):
       #   genome.fitness -= (output[0] - xo[0]) ** 2 
 
 
-    # potential wanted info:
-    # - inventory
-    # - turn number
-    # - robber position
-    # - roads + color_index
-    # - buildings + color_index
+    def build_settlement(self, game: Game, playable_actions_id_set):
+      #building appropriate input for the lobe
+      input_enemy_road_allocation = custom_state_functions.get_enemy_road_allocation(game, self.color)
+      input_trinary_building_ownership = custom_state_functions.get_trinary_building_ownership(game, self.color)
+      input_port_resource_allocation = custom_state_functions.get_port_resource_allocation(game)
+      input_tile_robber_allocation = custom_state_functions.get_tile_robber_allocation(game)
+      input_tile_resource_allocation = custom_state_functions.get_tile_resource_allocation(game)
+      input_tile_probabilities = custom_state_functions.get_tile_probabilities(game)
+      input_longest_road_trophy_allocation = custom_state_functions.get_longest_road_trophy_allocation(game, self.color)
+      input_all_player_longest_road_length = custom_state_functions.get_all_player_longest_road_length(game, self.color)
 
-    def build_settlement(game: Game, playable_actions: Iterable[Action]):
-      pass
+      # print("input_enemy_road_allocation", len(input_enemy_road_allocation))
+      # print("input_trinary_building_ownership", len(input_trinary_building_ownership))
+      # print("input_port_resource_allocation", len(input_port_resource_allocation))
+      # print("input_tile_robber_allocation", len(input_tile_robber_allocation))
+      # print("input_tile_resource_allocation", len(input_tile_resource_allocation))
+      # print("input_tile_probabilities", len(input_tile_probabilities))
+      # print("input_longest_road_trophy_allocation", len(input_longest_road_trophy_allocation))
+      # print("input_all_player_longest_road_length", len(input_all_player_longest_road_length))
+
+      input = list(itertools.chain(input_enemy_road_allocation, input_trinary_building_ownership, input_port_resource_allocation, input_tile_robber_allocation, input_tile_resource_allocation, input_tile_probabilities, input_longest_road_trophy_allocation, input_all_player_longest_road_length))
+      output = self.lobe_build_settlement.activate(input)
+      action = self.get_best_valid_action(output, ActionType.BUILD_SETTLEMENT, playable_actions_id_set)
+      return action
