f75c6ceefbb4d95e996b025c7794291baf91d2f5
David De Angelis <daviddeangelis98@gmail.com>
2022-05-20 02:00:57 +1000

    feat: refactoring and expanding formal genome evaluation sequence

--- catanatron_starNEAT.py
+++ catanatron_starNEAT.py
@@ -1,11 +1,11 @@
 from __future__ import print_function
 import math
 import time
+from datetime import datetime
 from catanatron.game import Color, Game
 from catanatron.models.player import RandomPlayer
-from catanatron.players import weighted_random
-from starNEAT.starNEAT import starNEAT
-from custom_state_functions import get_fitness_from_game_state
+from catanatron.players.weighted_random import WeightedRandomPlayer
+from catanatron.players.search import VictoryPointPlayer
 
 import os
 import sys
@@ -14,8 +14,11 @@ import random
 import multiprocessing
 import visualise.visualise as visualise
 
-from MyPlayer import MyBrain
-from MyPlayer import MyPlayer
+from MyPlayer import MyPlayer, MyBrain
+from starNEAT.starNEAT import starNEAT
+from custom_state_functions import get_fitness_from_game_state
+from opponents.minimax import AlphaBetaPlayer, ValueFunctionPlayer
+from reporters.AdvancedCheckpointer import AdvancedCheckpointer
 
 class Experiment(starNEAT):
   lobes = ["move_robber", "build_road", "build_settlement", "build_city", "play_year_of_plenty", "play_monopoly", "maritime_trade", "decide_action_type"]
@@ -34,17 +37,20 @@ class Experiment(starNEAT):
     # Add execution reporters (monitors the execution throughout its lifetime)
     self.add_reporters()
     best_genome = super().run()
+
     #perform post-run analysis
     self.wrap_up(best_genome)
     return best_genome
 
 
   def add_reporters(self):
+    time_stamp = datetime.now().strftime("%y-%m-%d-%H-%M")
+
     self.statistics_reporter = neat.StatisticsReporter()
     reporters = [
       neat.StdOutReporter(True),
       self.statistics_reporter,
-      neat.Checkpointer(generation_interval=100, filename_prefix='checkpoints/starNEAT-checkpoint-'),
+      AdvancedCheckpointer(generation_interval=50, time_interval_seconds=None, filename_prefix='checkpoints/{0}/starNEAT-checkpoint-'.format(time_stamp), epochs=self.epochs),
     ]
 
     for reporter in reporters:
@@ -69,7 +75,7 @@ class Experiment(starNEAT):
 
     while remaining_genomes > 0:
       batch_size = min(standard_batch_size, remaining_genomes)
-      start_index = batch_size * batch_index
+      start_index = standard_batch_size * batch_index
       end_index = start_index + batch_size
 
       genome_subset = genomes[start_index:end_index]
@@ -85,19 +91,22 @@ class Experiment(starNEAT):
     batch_fitnesses = [p.get() for p in processes]
 
     #Assign the calculated fitnesses from the multiprocessing to the genome objects.
-    for batch, batch_fitness in zip(batches, batch_fitnesses):
-      for (genome_id, genome), fitness in zip(batch, batch_fitness):
-        genome.fitness = fitness
+    fitnesses = {}
+    for batch_fitness in batch_fitnesses:
+      fitnesses.update(batch_fitness)
+
+    for genome_id, genome in genomes:
+      genome.fitness = fitnesses[genome_id]
       
     time_taken = time.time() - start_time
     print("Peformed 1000 games multithreadedly in", time_taken, "seconds...")
 
   @staticmethod
   def evaluate_genome_subset(genome_subset, genome_config, opponent_type, neural_network_type, lobes):
-    subset_fitness = []
+    subset_fitness = {}
     for genome_id, genome in genome_subset:
         fitness, games_won = Experiment.evaluate_genome(genome, genome_config, opponent_type, neural_network_type, lobes, num_games=10)
-        subset_fitness.append(fitness)
+        subset_fitness[genome_id] = fitness
 
     return subset_fitness
 
@@ -129,40 +138,56 @@ class Experiment(starNEAT):
         games_won += 1
       
     fitness = player_cumulative_fitness
-    # genome.fitness = 
     return (fitness, games_won)
 
 
   @staticmethod
   def play_game(brain, opponent_type):
     player = MyPlayer(brain, Color.BLUE) #User's player should always be BLUE.
-    opponent = opponent_type(Color.RED)
+    opponent = Experiment.get_opponent_from_type(opponent_type, Color.RED)
 
-    # player-order impacts win-likelihood significantly, especially with two players (as opposed to 4).
     players = [ player, opponent ]
-    random.shuffle(players)
 
     game = Game(players)
     winning_color = game.play()
-
-    #player_fitness = get_fitness_from_game_state(game, player.color)
-    #opponent_fitness = get_fitness_from_game_state(game, opponent.color)
-
-    # print("Winning color:", winning_color, "PLAYER FITNESS: ", player_fitness, "OPPONENT FITNESS: ", opponent_fitness)
     return game
-
-
-  def formally_evaluate_best_genome(self, best_genome):
-    num_games = 100
-    genome_fitness, games_won = self.evaluate_genome(best_genome, self.config.genome_config, RandomPlayer, self.neural_network_type, self.lobes, num_games=num_games)
-    print("The best genome won", games_won, "games out of", str(num_games), "against a RandomPlayer!")
-
 
   def wrap_up(self, best_genome):
     # Show output of the most fit genome against training data.
     print('\nOutput:')
-    self.formally_evaluate_best_genome(best_genome)
+    self.formally_evaluate_genome(best_genome, self.config.genome_config, self.neural_network_type, self.lobes)
     self.show_statistics()
+
+
+  @staticmethod
+  def formally_evaluate_genome(genome, genome_config, neural_network_type, lobes):
+    print("~~~~~")
+    print("Formally evaluating genome...")
+    print("~~~~~")
+    # Random
+    num_games = 1000
+    genome_fitness, games_won = Experiment.evaluate_genome(genome, genome_config, RandomPlayer, neural_network_type, lobes, num_games=num_games)
+    print("The best genome won", games_won, "games out of", str(num_games), "against Random Player!")
+
+    # VictoryPointPlayer
+    num_games = 1000
+    genome_fitness, games_won = Experiment.evaluate_genome(genome, genome_config, VictoryPointPlayer, neural_network_type, lobes, num_games=num_games)
+    print("The best genome won", games_won, "games out of", str(num_games), "against VictoryPoint Player!")
+
+    # WeightedRandom Player
+    num_games = 1000
+    genome_fitness, games_won = Experiment.evaluate_genome(genome, genome_config, WeightedRandomPlayer, neural_network_type, lobes, num_games=num_games)
+    print("The best genome won", games_won, "games out of", str(num_games), "against WeightedRandom Player!")
+
+    # ValueFunction
+    num_games = 25
+    genome_fitness, games_won = Experiment.evaluate_genome(genome, genome_config, ValueFunctionPlayer, neural_network_type, lobes, num_games=num_games)
+    print("The best genome won", games_won, "games out of", str(num_games), "against ValueFunction Player!")
+
+    # AlphaBeta(n=2) - CATANATRON
+    num_games = 25
+    genome_fitness, games_won = Experiment.evaluate_genome(genome, genome_config, AlphaBetaPlayer, neural_network_type, lobes, num_games=num_games)
+    print("The best genome won", games_won, "games out of", str(num_games), "against AlphaBeta(n=2) (CATANATRON)!")
 
 
   """
@@ -170,8 +195,23 @@ class Experiment(starNEAT):
     Return: A valid catan opponent type
   """
   def get_worthy_opponent_type(self): 
-    return RandomPlayer
+    return ValueFunctionPlayer
 
+  @staticmethod
+  def get_opponent_from_type(opponent_type, opponent_color):
+    if (opponent_type == RandomPlayer):
+      return RandomPlayer(opponent_color)
+    elif (opponent_type == VictoryPointPlayer):
+      return VictoryPointPlayer(opponent_color)
+    elif (opponent_type == WeightedRandomPlayer):
+      return WeightedRandomPlayer(opponent_color)
+    elif (opponent_type == ValueFunctionPlayer):
+      return ValueFunctionPlayer(opponent_color, is_bot=False)
+    elif (opponent_type == AlphaBetaPlayer):
+      return AlphaBetaPlayer(opponent_color, 2, True)
+    else:
+      raise Exception("Unrecognised Opponent Type:", opponent_type)
+    
 
   def show_statistics(self):
     try:
